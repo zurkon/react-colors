6b4baf8af2e7bb7d906f56bc4ac58a0223c39123
Zurkon <lukeyoshioka@gmail.com>
2020-12-21 00:06:39 -0300

    Added Route and Switch components from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './components/Palette/Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers.js';
 
 class App extends React.Component {
+
+  findPalette(id) {
+    return seedColors.find((palette) => palette.id === id);
+  }
+
   render() {
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        {/* <Route path="/palette/:id" render={(props) => {
+          console.log(props)
+        }} /> */}
+        <Route path="/palette/:id" render={(routeProps) => (<Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />)} />
+        <Route path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+      </Switch>
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
     );
   }
 }
