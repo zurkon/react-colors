ef36923accda92307775213ff2f2f2f7d38c2e5d
Zurkon <lukeyoshioka@gmail.com>
2020-12-26 18:05:48 -0300

    Added ColorPicker with react-color and connected with Material UI Button component

--- src/components/PaletteForm/PaletteForm.js
+++ src/components/PaletteForm/PaletteForm.js
@@ -8,13 +8,18 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+
+import { ChromePicker } from 'react-color';
 
 import useStyles from './PaletteForm.styles';
 
 const PaletteForm = () => {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [currentColor, setCurrentColor] = React.useState('#22CFCF');
+  const [colors, setColors] = React.useState(['purple', '#e15764']);
+  const [open, setOpen] = React.useState(true);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -23,6 +28,14 @@ const PaletteForm = () => {
   const handleDrawerClose = () => {
     setOpen(false);
   };
+
+  const updateCurrentColor = (newColor) => {
+    setCurrentColor(newColor.hex);
+  }
+
+  const addNewColor = () => {
+    setColors([...colors, currentColor]);
+  }
 
   return (
     <div className={classes.root}>
@@ -63,6 +76,24 @@ const PaletteForm = () => {
           </IconButton>
         </div>
         <Divider />
+        <Typography variant="h4">
+          Design Your Palette
+        </Typography>
+        <div>
+          <Button variant="contained" color="secondary">Clear Palette</Button>
+          <Button variant="contained" color="primary">Random Color</Button>
+        </div>
+        <ChromePicker
+          color={currentColor}
+          disableAlpha
+          onChange={updateCurrentColor}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ backgroundColor: currentColor }}
+          onClick={addNewColor}
+        >Add Color</Button>
       </Drawer>
       <main
         className={clsx(classes.content, {
@@ -70,6 +101,13 @@ const PaletteForm = () => {
         })}
       >
         <div className={classes.drawerHeader} />
+        <ul>
+          {
+            colors.map(color => (
+              <li style={{ backgroundColor: color }}>{color}</li>
+            ))
+          }
+        </ul>
       </main>
     </div>
   );
